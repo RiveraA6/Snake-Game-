bddf785c92386b05b80b5a33c2862a59e8db6bbf
CodeSandbox <support@codesandbox.io>
2024-01-16 14:57:47 +0000

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import "./App.css";
-// import Snake from "./Snake.ts";
+import Snake from "./Snake";
 import display from "./display";
 import { useEffect } from "react";
 
@@ -8,8 +8,19 @@ export default function App() {
   // Add Snake Tests with display below
   useEffect(() => {
     // Include your display statements to test below
+    const smallSnake = new Snake("green");
+    const bigSnake = new Snake("red");
     document.getElementById("output")!.innerText = "OUTPUT:\n";
     display("hey");
+    display(smallSnake.Position);
+    display(bigSnake.Position);
+    smallSnake.move(4);
+    bigSnake.move(8);
+    display(smallSnake.Position);
+    display(bigSnake.Position);
+    smallSnake.turn();
+    smallSnake.move(2);
+    display(smallSnake.Position);
   }, []);
   return (
     <div className="App">
--- src/Snake.ts
+++ src/Snake.ts
@@ -2,6 +2,32 @@
 
 // place your code on line 5 above the export statement below
 
-class Snake {}
+class Snake {
+  private currentPosition: number;
+  private currentDirection: number;
+  public color: string;
+  constructor(snakecolor: string) {
+    this.currentPosition = 0;
+    this.currentDirection = 1;
+    this.color = snakecolor;
+  }
+  public move(numberOfSteps: number): void {
+    if (this.currentDirection === 1) {
+      this.currentPosition += numberOfSteps;
+    } else if (this.currentDirection === -1) {
+      this.currentPosition -= numberOfSteps;
+    }
+  }
+  public turn(): void {
+    if (this.currentDirection === 1) {
+      this.currentDirection = -1;
+    } else if (this.currentDirection === -1) {
+      this.currentDirection = 1;
+    }
+  }
+  public get Position(): number {
+    return this.currentPosition;
+  }
+}
 
 export default Snake;
